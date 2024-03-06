da730730f10ba578d86164dc20b9916d667b9674
kaipapar <120021905+kaipapar@users.noreply.github.com>
2024-03-06 11:45:43 +0200

    5.17 done
    test inits the DB every time the tests are run.
    it also checks that all of the login form elements are visible

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -2,14 +2,14 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
-/*     await request.post('http:localhost:3001/api/testing/reset')
+    await request.post('http:localhost:3001/api/testing/reset')
     await request.post('http://localhost:3001/api/users', {
       data: {
         name: 'Matti Luukkainen',
         username: 'mluukkai',
         password: 'salainen'
       }
-    }) */
+    }) 
 
     await page.goto('http://localhost:5173')
   })
